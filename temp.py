88e8b553e2106ff6e9ea7b743028700f37e6590c
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-02-09 13:22:38 +0530

    Fac

--- temp.py
+++ temp.py
@@ -3,6 +3,13 @@ def sum_back(prev_sum, num):
         return prev_sum
     return sum_back(prev_sum + num, num - 1)
 
-print(sum_back(0,100))
+# print(sum_back(0,100))
 
-print(100 * (100 + 1) // 2)
+# print(100 * (100 + 1) // 2)
+
+def fac(res ,num):
+    if num == 0 or num == 1:
+        return res
+    return fac(res * num, num - 1)
+
+print(fac(1, 6))
