d742e67af92ee3469353e1dc02f00f9f70bbb0ca
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-04-09 21:23:12 +0530

    Adding Circular LinkList to knowl;ege base

---
+++ CircularLL.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+    
+    def __len__(self):
+        return self.size
+
